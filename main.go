8a5d87f9531a8ed2ff2c8a64acdebaae43af7c52
yamodah <yassinedahlek@gmail.com>
2022-03-27 18:44:45 -0400

    basic api setup and live time for details

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+ func getStreams(w http.ResponseWriter, r *http.Request){
+	 fmt.Fprint(w,"hakuna matata")
+ }
+ func main(){
+	 http.HandleFunc("/", getStreams)
+	 http.ListenAndServe(":5000", nil)
+ }
