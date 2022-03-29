d9ed3fee17dec8206cb105c616a2e0236cd33d9e
yamodah <yassinedahlek@gmail.com>
2022-03-29 13:07:31 -0400

    fixed instructions

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ import (
 
 	 switch r.URL.Path {
 	 case "/":
-		 fmt.Fprint(w,"Welcome to Yamodah's Streams ! \n to receive links for a stream query in url by adding '/all?stream=' followed by the name of a name of a team, event etc. \n for example looking for a fight enter a fighters last name, a football match enter a team name for the query. \n this is a work in progress but please enjoy!")
+		 fmt.Fprint(w," Welcome to Yamodah's Streams ! \n to receive links for a stream, query in url by adding '/all?stream=' followed by the name of a live event. \n for example looking for a fight enter a fighters last name, a football match enter a team name for the query. \n this is a work in progress but please enjoy!")
 	 case "/all":
 		rojaScrape(w,c,desiredEvent)
 	// 	liveTvScrape(w)
